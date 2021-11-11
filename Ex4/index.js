833208575312b91c31a0639625742a28f17d894d
Tucker <90000281+sizzub@users.noreply.github.com>
2021-11-11 16:25:12 -0600

    Update index.js

--- Ex4/index.js
+++ Ex4/index.js
@@ -1,5 +1,5 @@
 function apply() {
-  var par = document.getElementById("paragraph");
+  var par = document.getElementById("myPar");
   var r1 = document.getElementById("red1").value;
   var g1 = document.getElementById("green1").value;
   var b1 = document.getElementById("blue1").value;
@@ -7,8 +7,8 @@ function apply() {
   var r2 = document.getElementById("red2").value;
   var g2 = document.getElementById("green2").value;
   var b2 = document.getElementById("blue2").value;
-  
-  par.style.borderColor = rgb(r1, g1, b1);
+
+  par.style.borderColor = "rgb(" + r1 +", " + g1 + ", " + g1 + ")";
   par.style.borderWidth = width;
-  par.style.backgroundColor = rgb(r2, g2, b2);
+  par.style.backgroundColor = "rgb(" + r2 +", " + g2 + ", " + b2 + ")";
 }
